bbfea4705d0c825e8e931cffafd2794e4d314392
sguschin <sguschin@gear-games.com>
2026-01-30 23:51:43 +0400

    IMoL: Task 2 add. - fix logic

--- InvisibleMechanismsOfLogic/Task2_BankAcount/BankAccount.cs
+++ InvisibleMechanismsOfLogic/Task2_BankAcount/BankAccount.cs
@@ -2,16 +2,58 @@
 
 public class BankAccount
 {
+    private readonly string AMOUNT_LESS_THAN_ZERO_MESSAGE = "Balance cannot be negative";
+    private readonly string INSUFFICIENT_FUNDS_MESSAGE = "Balance cannot be negative";
+    private readonly string OVERFLOW_FUNDS_MESSAGE = "The account balance has been exceeded";
     private double _balance;
 
-    public BankAccount(double balance) => _balance = balance;
-    
-    
-    public void Deposit(double amount) => _balance += amount;
-    
-    
-    public void Withdraw(double amount) => _balance -= amount;
+    public BankAccount(double balance)
+    {
+        AssertAmountGreaterThanZero(balance, AMOUNT_LESS_THAN_ZERO_MESSAGE);
+        
+        _balance = balance;
+    }
+
+
+    public void Deposit(double amount)
+    {
+        AssertAmountGreaterThanZero(amount, AMOUNT_LESS_THAN_ZERO_MESSAGE);
+
+        double newBalance;
+
+        try
+        {
+            checked
+            {
+                newBalance = _balance + amount;
+            }
+        }
+        catch
+        {
+            throw new InvalidOperationException(OVERFLOW_FUNDS_MESSAGE);
+        }
+        
+        _balance = newBalance;
+    }
+
+
+    public void Withdraw(double amount)
+    {
+        AssertAmountGreaterThanZero(amount, AMOUNT_LESS_THAN_ZERO_MESSAGE);
+        
+        double newBalance = _balance - amount;
+        
+        AssertAmountGreaterThanZero(newBalance, INSUFFICIENT_FUNDS_MESSAGE);
+        
+        _balance = newBalance;
+    }
     
     
     public double GetBalance() => _balance;
+
+    private void AssertAmountGreaterThanZero(double amount, string message)
+    {
+        if (amount < 0.0d) 
+            throw new InvalidOperationException(message);
+    }
 }
