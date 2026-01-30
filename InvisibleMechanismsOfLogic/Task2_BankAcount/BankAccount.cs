f3538a1aaceb634df369c023ccafb68ed43f1c17
sguschin <sguschin@gear-games.com>
2026-01-30 23:31:32 +0400

    IMoL: Task 2 - BankAccount

---
+++ InvisibleMechanismsOfLogic/Task2_BankAcount/BankAccount.cs
@@ -0,0 +1,17 @@
+﻿namespace InvisibleMechanismsOfLogic.Task2_BankAcount;
+
+public class BankAccount
+{
+    private double _balance;
+
+    public BankAccount(double balance) => _balance = balance;
+    
+    
+    public void Deposit(double amount) => _balance += amount;
+    
+    
+    public void Withdraw(double amount) => _balance -= amount;
+    
+    
+    public double GetBalance() => _balance;
+}
---
+++ InvisibleMechanismsOfLogic/Task2_BankAcount/TestBankAccount.cs
@@ -0,0 +1,13 @@
+﻿namespace InvisibleMechanismsOfLogic.Task2_BankAcount;
+
+public class TestBankAccount
+{
+    public static void Main(string[] args)
+    {
+        BankAccount account = new BankAccount(1000);
+        account.Deposit(500);
+        account.Withdraw(200);
+        
+        Console.WriteLine($"Balance: {account.GetBalance()}");
+    } 
+}
