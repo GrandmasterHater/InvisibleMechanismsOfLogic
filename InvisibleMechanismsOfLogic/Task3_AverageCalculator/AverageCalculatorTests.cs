81800879d4dbc1606a8fae7779fa9d795b05d110
sguschin <sguschin@gear-games.com>
2026-01-31 13:43:55 +0400

    IMoL: Task 3 - fix test

--- InvisibleMechanismsOfLogic/Task3_AverageCalculator/AverageCalculatorTests.cs
+++ InvisibleMechanismsOfLogic/Task3_AverageCalculator/AverageCalculatorTests.cs
@@ -11,18 +11,10 @@ namespace InvisibleMechanismsOfLogic.Task3_AverageCalculator
             Assert.Throws<ArgumentNullException>(() => AverageCalculator.CalculateAverage(null));
         }
         
-        [Test]
-        public void CalculateAverage_WhenArrayIsEmpty_ReturnsZero()
-        {
-            int[] array = [1, 2, 3, 4];
-            double expectedAverage = 2.5d;
-            
-            Assert.That(AverageCalculator.CalculateAverage(array), Is.EqualTo(expectedAverage));
-        }
-        
         // Не покрыт случай переполнения переменно при подсчёте среднего. Реализация может быть не статистическая, 
         // а наивная, в лоб, когда подсчёт суммы ведется в переменную типа int. Тогда в массиве, содержащем минимум 2 
         // значения int.MaxValue, при суммировании произойдёт переполнение и подсчёт будет некорректным.
+        [TestCase(new int[]{}, 0d)]
         [TestCase(new int[]{1}, 1d)]
         [TestCase(new int[]{1, 2}, 1.5d)]
         [TestCase(new int[]{1, 2, 3, 4}, 2.5d)]
