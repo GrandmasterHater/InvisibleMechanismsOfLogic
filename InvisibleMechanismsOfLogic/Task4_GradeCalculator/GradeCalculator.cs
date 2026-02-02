9017cb4e069bfefda9ab5e2b8f4d1d2111fa286f
sguschin <sguschin@gear-games.com>
2026-02-02 10:51:09 +0400

    IMoL: Task 4 - GradesCalulator

---
+++ InvisibleMechanismsOfLogic/Task4_GradeCalculator/GradeCalculator.cs
@@ -0,0 +1,63 @@
+﻿namespace InvisibleMechanismsOfLogic.Task4_GradeCalculator;
+
+/*
+Имеется класс GradeCalculator с методом calculateAverage(List grades), который вычисляет среднее значение оценок студентов.
+Реализуйте этот класс с учётом того, что существуют граничные случаи, которые необходимо правильно обработать, чтобы избежать логических ошибок.
+Сделайте пять концептуально различающихся тестов для метода calculateAverage.
+*/
+public class GradeCalculator
+{
+    public AverageResult CalculateAverage(List<int> grades)
+    {
+        if (grades is null)
+            return new AverageResult(AverageResultStatus.ARGUMENT_NULL_STATUS);
+        
+        if (grades.Count == 0)
+            return new AverageResult(AverageResultStatus.EMPTY_GRADE_LIST);
+        
+        if (!AssertAllGradesGreaterOrEqualZero(grades))
+            return new AverageResult(AverageResultStatus.GRADE_LESS_THAN_ZERO);
+        
+        double avarege = 0.0d;
+
+        for (int i = 0; i < grades.Count; i++)
+        {
+            avarege += (grades[i] - avarege) / (i + 1);
+        }
+
+        return new AverageResult(avarege, AverageResultStatus.SUCCESS);
+    }
+
+    private bool AssertAllGradesGreaterOrEqualZero(List<int> grades)
+    {
+        return grades.All(grade => grade >= 0);
+    }
+    
+    public struct AverageResult
+    {
+        public double Average { get; }
+        
+        public AverageResultStatus Status { get; }
+
+        public AverageResult(AverageResultStatus status)
+        {
+            Average = 0.0d;
+            Status = status;
+        }
+        
+        public AverageResult(double average, AverageResultStatus status)
+        {
+            Average = average;
+            Status = status;
+        }
+    }
+    
+    public enum AverageResultStatus
+    {
+        NONE = 0,
+        SUCCESS,
+        EMPTY_GRADE_LIST,
+        GRADE_LESS_THAN_ZERO,
+        ARGUMENT_NULL_STATUS
+    }
+}
---
+++ InvisibleMechanismsOfLogic/Task4_GradeCalculator/GradeCalculatorTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+
+namespace InvisibleMechanismsOfLogic.Task4_GradeCalculator;
+
+[TestFixture]
+public class GradeCalculatorTests
+{
+    [TestCaseSource(nameof(NotSuccessAverageCases))]
+    [TestCaseSource(nameof(SuccessAverageCases))]
+    public GradeCalculator.AverageResult CalculateAverage_WhenReturnsNotSuccessStatuses(List<int> grades)
+    {
+        GradeCalculator gradeCalculator = CreateGradeCalculator();
+
+        return gradeCalculator.CalculateAverage(grades);
+    }
+
+    private static IEnumerable<TestCaseData> NotSuccessAverageCases()
+    {
+        yield return new TestCaseData(null)
+            .Returns(new GradeCalculator.AverageResult(0d, GradeCalculator.AverageResultStatus.ARGUMENT_NULL_STATUS))
+            .SetName("CalculateAverage_WhenArgumentNull_ReturnsArgumentNullStatus");
+        yield return new TestCaseData(new List<int>())
+            .Returns(new GradeCalculator.AverageResult(0d, GradeCalculator.AverageResultStatus.EMPTY_GRADE_LIST))
+            .SetName("CalculateAverage_WhenGradeListEmpty_ReturnsEmptyGradeListStatus");
+        yield return new TestCaseData(new List<int> {1, -1})
+            .Returns(new GradeCalculator.AverageResult(0d, GradeCalculator.AverageResultStatus.GRADE_LESS_THAN_ZERO))
+            .SetName("CalculateAverage_WhenAnyGradeLessThanZero_ReturnsGradeLessThanZeroStatus");
+    }
+    
+    private static IEnumerable<TestCaseData> SuccessAverageCases()
+    {
+        int grade = int.MaxValue;
+        double expectedGrade = Convert.ToDouble(grade);
+        
+        yield return new TestCaseData(new List<int>() { grade, grade })
+            .Returns(new GradeCalculator.AverageResult(expectedGrade, GradeCalculator.AverageResultStatus.SUCCESS))
+            .SetName("CalculateAverage_WhenHasSeveralMaxIntGrades_ReturnsSuccessStatusAndAverage");
+
+        grade = 5;
+        expectedGrade = Convert.ToDouble(grade);
+        
+        yield return new TestCaseData(new List<int> {grade})
+            .Returns(new GradeCalculator.AverageResult(expectedGrade, GradeCalculator.AverageResultStatus.SUCCESS))
+            .SetName("CalculateAverage_WhenHasSingleElement_ReturnsSuccessStatusAndAverage");
+
+        List<int> grades = new List<int> { 0, 1, 2, 3, 4 };
+        double expectedGradesAverage = 2d;
+        
+        yield return new TestCaseData(grades)
+            .Returns(new GradeCalculator.AverageResult(expectedGradesAverage, GradeCalculator.AverageResultStatus.SUCCESS))
+            .SetName("CalculateAverage_WhenSeveralGreaterOrZeroGrades_ReturnsSuccessStatusAndAverage");
+    }
+    
+    private GradeCalculator CreateGradeCalculator() => new();
+}
