30a4f311cbd16313dab010390a7b1bb3c8343309
sguschin <sguschin@gear-games.com>
2026-02-06 23:14:45 +0400

    IMoL: Task 8 - пример многопоточности

---
+++ InvisibleMechanismsOfLogic/Task8_MultyThreadExample/MultyThreadExample.cs
@@ -0,0 +1,36 @@
+﻿namespace InvisibleMechanismsOfLogic.Task8_MultyThreadExample;
+
+public class MultyThreadExample
+{
+    private static int counter = 0;
+
+    public static void Run()
+    {
+        ThreadStart task = () =>
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                // Исправление: заменяет небехопасный инкремент counter++ на безопасный.
+                Interlocked.Increment(ref counter);
+            }
+        };
+
+        Thread thread1 = new Thread(task);
+        Thread thread2 = new Thread(task);
+
+        thread1.Start();
+        thread2.Start();
+
+        try
+        {
+            thread1.Join();
+            thread2.Join();
+        }
+        catch (ThreadInterruptedException e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        Console.WriteLine("Counter: " + counter);
+    }
+}
