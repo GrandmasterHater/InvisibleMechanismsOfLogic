96ef4dab246b0aa1799b5778fcabd137aaeb5ca4
sguschin <sguschin@gear-games.com>
2026-02-11 14:07:00 +0400

    IMoL: Task12 - HoareTriple

---
+++ InvisibleMechanismsOfLogic/Task12_HoareTriple/HoareTripleExample.cs
@@ -0,0 +1,28 @@
+﻿namespace InvisibleMechanismsOfLogic.Task12_HoareTriple;
+
+public class HoareTripleExample
+{
+    // { P: x != int.MinValue }
+    // { Q: result >= 0 ∧ ((x < 0 => result = -x) v (x >= 0 => result = x)) }
+    public int Abs(int x)
+    {
+        if (x == int.MinValue) 
+            throw new ArgumentException("The value must be a not equal to int.MinValue.");
+        
+        return x < 0 ? -x : x;
+    }
+
+    // { P: true }
+    // { Q: (result = x v result = y) ∧ result >= x ∧ result >= y }
+    public int Max(int x, int y)
+    {
+        return x < y ? y : x;
+    }
+
+    // { P: x != int.MinValue ∧ y != int.MinValue }
+    // { Q: result >= 0 ∧ (result = |x| v result = |y|) ∧ result >= |x| ∧ result >= |y| }
+    public int CombineAbsAndMax(int x, int y)
+    {
+        return Max(Abs(x), Abs(y));
+    }
+}
